fcdb8f2598efcca87c74137d63b48a8e2a8d0e15
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2018-09-16 17:44:20 +0300

    feat: Handle user authentication using Firebase

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 export default class login extends Component {
-	state = { email: '', password: '' };
+	state = { email: '', password: '', error: '' };
+
+	onButtonPress() {
+		const { email, password } = this.state;
+
+		this.setState({ error: '' }); // to reset error message.
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.catch(() => {
+						this.setState({ error: 'Authentication failed.' });
+					});
+			});
+	}
 
 	render() {
 		return (
@@ -26,10 +42,24 @@ export default class login extends Component {
 					/>
 				</CardSection>
 
+				<Text style={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
 				<CardSection>
-					<Button>Login</Button>
+					<Button onPress={this.onButtonPress.bind(this)} >
+						Login
+					</Button>
 				</CardSection>
 			</Card>
 		);
 	}
 }
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
